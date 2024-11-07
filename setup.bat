b66afd128218f8e3ab4539cc676b8a7c0cd7ed50
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2024-11-07 12:42:31 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,3 @@
 echo "I am still actively developing this project and, it's not currently in a usable state. Please email me for questions/comments/concerns - jpatrick5402@gmail.com"
+
+REM git clone https://github.com/nmap/npcap
