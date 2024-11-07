04436ce2cd13f269ecf3f42170dde5179f6a71db
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2024-11-07 14:03:53 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 echo "I am still actively developing this project and, it's not currently in a usable state. Please email me for questions/comments/concerns - jpatrick5402@gmail.com"
 
 # git clone https://github.com/the-tcpdump-group/libpcap
+# sudo apt-get install libpcap-dev
