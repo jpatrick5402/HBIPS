311fffc4a701462c94ef25cee2737e0e08075fb5
jpatrick5402 <jpatrick5402@gmail.com>
2024-11-07 14:23:37 -0500

    Formatting adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,17 @@
 int main(int argc,char *argv[])
 {
     char error[PCAP_ERRBUF_SIZE];
-    pcap_if_t *interfaces,*temp;
-    int i=0;
-    if(pcap_findalldevs(&interfaces,error)==-1) {
-        printf("\nerror in pcap findall devs");
-        return -1;   
+    pcap_if_t * interfaces, * temp;
+    int i = 0;
+    if (pcap_findalldevs(&interfaces,error) == -1) {
+        printf("Error in pcap findall devs\n");
+        return -1;
     }
 
-    printf("\n the interfaces present on the system are:");
-    for(temp=interfaces;temp;temp=temp->next) {
-        printf("\n%d  :  %s",i++,temp->name);
+    printf("The interfaces present on the system are:\n");
+    for(temp = interfaces; temp ; temp = temp->next) {
+        printf("%d  :  %s\n", i++, temp->name);
     }
 
-    printf("\n");
     return 0;
 }
