f66e67ace3a73db227876088b8692c8b112920b0
Joseph Patrick <68129149+jpatrick5402@users.noreply.github.com>
2024-11-07 14:57:24 -0500

    Delete deb_setup.sh

--- deb_setup.sh
+++
@@ -1,5 +0,0 @@
-echo "I am still actively developing this project and, it's not currently in a usable state. Please email me for questions/comments/concerns - jpatrick5402@gmail.com"
-
-# sudo apt-get install libpcap-dev
-# sudo apt install g++
-# 
