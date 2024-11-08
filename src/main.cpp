06cbbd82a90275cc308089d80239461795b3a811
jpatrick5402 <jpatrick5402@gmail.com>
2024-11-07 21:17:58 -0500

    Adding a configuration menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "print_network_devices.h"
+#include "display_configuration_menu.h"
+
+using namespace std;
 
 int main (int argc, char *argv[]) {
     print_network_devices();
+
+    unordered_map<string, int> configs = display_configuration_menu(); // choose configuration items
+
+    // loop through program catpuring packets and reporting issues
+
     return 0;
 }
