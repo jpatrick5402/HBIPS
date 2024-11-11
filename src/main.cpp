b332777b8b3590be095530fe05a9c5c4080ebf38
jpatrick5402 <jpatrick5402@gmail.com>
2024-11-11 16:58:55 -0500

    Adding decision tree for command

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <string.h>
+#include <pcap/pcap.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
+
 #include "print_network_devices.h"
 #include "get_configs.h"
 
@@ -15,8 +23,27 @@ int main (int argc, char *argv[]) {
 |__/  |__/|_______/ |______/|__/       \______/
 )"""" << '\n';
 
-    print_network_devices();
-    cout << '\n';
+    bool run_in_foreground;
+
+    if (strcmp(argv[1], "-f") == 0) {
+        run_in_foreground = true;
+    } else if (strcmp(argv[1], "-b") == 0) {
+        run_in_foreground = false;
+    } else if (strcmp(argv[1], "-p") == 0) {
+        print_network_devices();
+        cout << '\n';
+        exit(0);
+    } else {
+        cout << "usage: " << argv[0] << " [option]" << '\n';
+        cout << "options: " << '\n';
+        cout << setw(10) << "-h" << ": display this help screen" << '\n';
+        cout << setw(10) << "-f" << ": run this application in forground" << '\n';
+        cout << setw(10) << "-b" << ": run this application in background" << '\n';
+        cout << setw(10) << "-p" << ": print network devices" << '\n';
+        exit(0);
+    }
+
+
     unordered_map<string, int> configs = get_configs(); // choose configuration items
     cout << '\n';
 
